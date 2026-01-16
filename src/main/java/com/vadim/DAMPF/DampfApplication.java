b35b23fee048b28851ad3c188aa6ce8fe9aa16a2
10136833 <vadimsedov2007@gmail.com>
2026-01-16 12:00:50 +0100

    Initial commit with spring initialezer

---
+++ src/main/java/com/vadim/DAMPF/DampfApplication.java
@@ -0,0 +1,13 @@
+package com.vadim.DAMPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DampfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DampfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vadim/DAMPF/DampfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadim.DAMPF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DampfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
