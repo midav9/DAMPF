407ce948e9c56c739e0ae1acf0c403105202d74d
10136833 <vadimsedov2007@gmail.com>
2026-02-02 10:43:41 +0100

    Added api interfacer "api/benutzer" for BenutzerController

--- src/main/java/com/vadim/DAMPF/controller/BenutzerController.java
+++ src/main/java/com/vadim/DAMPF/controller/BenutzerController.java
@@ -1,4 +1,11 @@
 package com.vadim.DAMPF.controller;
+import com.vadim.DAMPF.service.BenutzerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestController
+@RequestMapping("api/benutzer")
 
 public class BenutzerController {
 
